d238329cc2fc88df34281c83f2b8a9b6b2f428cc
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2019-01-13 01:39:29 -0300

    Finished python script

--- assets/dev/get_nodes.py
+++ assets/dev/get_nodes.py
@@ -5,6 +5,7 @@ Pillow: https://pillow.readthedocs.io/en/stable/reference/ImageDraw.html?highlig
 csv: https://docs.python.org/2/library/csv.html
 """
 
+
 def color_to_html(color):
     return '#{:06X}'.format((color[0] << 16) + (color[1] << 8) + color[2])
 
@@ -12,9 +13,29 @@ def color_to_html(color):
 def is_bounding_pixel(origin, pixel):
     return (abs(pixel[0] - origin[0]) <= brush_size) and (abs(pixel[1] - origin[1]) <= brush_size)
 
+def check_linear_path(start,end,step):
+    pos = [0,0]
+    subi = 1
+    if start[0]==end[0]:
+        pos[0] = start[0]
+        subi = 1
+    else:
+        pos[1] = start[1]
+        subi = 0
+    #print("Testing range: {start[0]};{start[1]} - {end[0]};{end[1]}".format(start=start,end=end))
+    #print("Constant is {}".format(start[not subi]))
+    for i in range(start[subi] + step*8, end[subi], step):
+        pos[subi] = i
+        #print("Checking {}".format(pos))
+        if color_to_html(im.getpixel( (pos[0],pos[1]) )) == node:
+            return True
+    return False
 
+wall = "#1E1EE4"
+node = "#FFFF00"
 brush_size = 8
 colored = []
+colored_dirs = []
 
 im = Image.open("./board_nodes.png")
 draw = ImageDraw.Draw(im)
@@ -22,7 +43,7 @@ draw = ImageDraw.Draw(im)
 for y in range(0, im.height):
     for x in range(0, im.width):
         pixel = im.getpixel((x, y))
-        if (color_to_html(pixel)) == "#FFFF00":
+        if (color_to_html(pixel)) == node:
             skip = False
             for c in colored:
                 if is_bounding_pixel(c, (x, y)):
@@ -35,17 +56,29 @@ for y in range(0, im.height):
 
 print("List {}\nLen: {}".format(colored, colored.__len__()))
 
+for p in colored:
+    possible = [
+        check_linear_path(p,[p[0],0],-1),
+        check_linear_path(p,[im.width,p[1]],1),
+        check_linear_path(p,[p[0],im.height],1),
+        check_linear_path(p,[0,p[1]],-1)]
+    colored_dirs.append(possible)
+
+
 with open("../nodes.csv", "w") as file:
-    writer = csv.DictWriter(file,fieldnames=["x","y","up","right","down","left"])
+    writer = csv.DictWriter(
+        file, fieldnames=["x", "y", "up", "right", "down", "left"])
     writer.writeheader()
-    for p in colored:
+    for i in range(0,colored.__len__()):
+        c = colored[i]
+        cd = colored_dirs[i]
         writer.writerow({
-            "x": p[0]+(brush_size/2),
-            "y": p[1]+(brush_size/2),
-            "up": True,
-            "right": True,
-            "down": True,
-            "left": True
+            "x": c[0]+(brush_size/2),
+            "y": c[1]+(brush_size/2),
+            "up": cd[0],
+            "right": cd[1],
+            "down": cd[2],
+            "left": cd[3]
         })
 
 
