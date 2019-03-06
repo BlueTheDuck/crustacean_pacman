76584e787cf567ee60626d00646223bff45536bc
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2019-03-06 15:21:42 -0300

    Finished get_dots.py dots.csv is in a usable state now TODO: Fix some issues on the imaged used to generate the dots file

--- assets/dev/nodes/get_dots.py
+++ assets/dev/nodes/get_dots.py
@@ -1,5 +1,9 @@
-from PIL import Image, ImageDraw, ImageColor
+from PIL import Image, ImageDraw, ImageColor, ImageMode
+from math import floor
+from tqdm import tqdm
+from sys import exit
 import csv
+
 """
 Pillow: https://pillow.readthedocs.io/en/stable/reference/ImageDraw.html?highlight=draw_arc
 csv: https://docs.python.org/2/library/csv.html
@@ -10,87 +14,92 @@ def color_to_html(color):
     return '#{:06X}'.format((color[0] << 16) + (color[1] << 8) + color[2])
 
 
-def is_bounding_pixel(origin, pixel):
-    return (abs(pixel[0] - origin[0]) <= brush_size) and (abs(pixel[1] - origin[1]) <= brush_size)
+def html_to_color(html):
+    n = int(html.replace("#", ""), 16)
+    return (n >> 16 & 0xFF, n >> 8 & 0xFF, n & 0xFF)
 
 
-
-def check_linear_path(start, end, step):
-    pos = [0, 0]
-    subi = 1
-    if start[0] == end[0]:
-        pos[0] = start[0]
-        subi = 1
-    else:
-        pos[1] = start[1]
-        subi = 0
-    for i in range(start[subi] + step*8, end[subi], step):
-        pos[subi] = i
-        pixel_color = im.getpixel((pos[0], pos[1]))
-        if pixel_color[2] > 0x0F and color_to_html(pixel_color)!=node:
-            print("({start[0]};{start[1]}) failed on ({pos[0]};{pos[1]})".format(start=start,pos=pos))
-            return False
-        if color_to_html(pixel_color)==node:
-            return True
-    return True
+def as_index(x, y):
+    if x < 0 or x > width:
+        raise IndexError("Out of range")
+    return y * width + x
 
 
-wall = "#1E1EE4"
-node = "#FF0000"
-brush_size = 8
-colored = []
-colored_dirs = []
+def as_coord(i):
+    return (i % width, floor(i/width))
 
+
+colors = {}
+colors["red"] = "#FF0000"
+colors["blue"] = "#1E1EE4"
+colors["green"] = "#00FF00"
+colors["yellow"] = "#FFFF00"
+colors["black"] = "#000000"
+colors["node"] = colors["red"]
 im = Image.open("./board+dots.png")
+width = im.width
+height = im.height
+pixelcount = width * height
+bitmap = [False]*(height * height)
 draw = ImageDraw.Draw(im)
 
-for y in range(0, im.height):
-    for x in range(0, im.width):
-        pixel = im.getpixel((x, y))
-        if (color_to_html(pixel)) == node:
-            skip = False
-            for c in colored:
-                if is_bounding_pixel(c, (x, y)):
-                    skip = True
-                    break
-            if skip:
-                continue
-            colored.append([int(x+(brush_size/2)), int(y+(brush_size/2))])
+im2 = Image.new("RGB", (width, height))
 
-print("List {}\nLen: {}".format(colored, colored.__len__()))
+print("{w}x{h}".format(w=width, h=height))
+""" for i in tqdm(range(0, pixelcount), desc="Checking program status"):
+    (x, y) = as_coord(i)
+    j = as_index(x, y)
+    try:
+        assert(j == i)
+        assert(x >= 0 and x < width and y >= 0 and y < height)
+    except AssertionError:
+        print("\n\t({x: 04},{y: 04}) = as_coord({i: 8})\nas_index({x: 04},{y: 04}) =        {j: 10}\n0 <= {x: 4} < {w}\n0 <= {y: 4} < {h}".format(
+            x=x, y=y, j=j, i=i, w=width, h=height))
+        exit(-1) """
 
-""" for p in colored:
-    print("Testing node ({p[0]};{p[1]})".format(p=p))
-    possible = [
-        check_linear_path(p, [p[0], 0], -1),
-        check_linear_path(p, [im.width, p[1]], 1),
-        check_linear_path(p, [p[0], im.height], 1),
-        check_linear_path(p, [0, p[1]], -1)]
-    colored_dirs.append(possible)
-print(colored_dirs) """
 
-with open("../nodes.csv", "w") as file:
+""" Preprocess: RGB > Boolean """
+for i in tqdm(range(0, pixelcount), desc="Converting to bitmap"):
+    (x, y) = as_coord(i)
+    pixel = im.getpixel((x, y))
+    bitmap[i] = (color_to_html(pixel) == colors["node"])
+    """ if bitmap[i]:
+        im2.putpixel((x, y), html_to_color(colors["node"])) """
+
+""" Analyze bitmap: True+True+True > True+False+False """
+for j in tqdm(range(0, pixelcount), desc="Analyzing bitmap"):
+    i = pixelcount - j
+    (x, y) = as_coord(i)
+    if x > 0 and bitmap[as_index(x-1, y)] == True:
+        bitmap[as_index(x, y)] = False
+        """ im2.putpixel((x, y), html_to_color(colors["black"])) """
+    if y > 0 and bitmap[as_index(x, y-1)] == True:
+        bitmap[as_index(x, y)] = False
+        """ im2.putpixel((x, y), html_to_color(colors["black"])) """
+
+""" Fix bitmap positions: True+False+False > False+True+False """
+for j in tqdm(range(0, pixelcount), desc="Fixing positions"):
+    i = pixelcount - j
+    (x, y) = as_coord(i)
+    if bitmap[as_index(x, y)] == True:
+        bitmap[as_index(x, y)] = False
+        bitmap[as_index(x+3, y+3)] = True
+        """ im2.putpixel((x, y), (0xFF, 0xFF, 0x0))
+        try:
+            im2.putpixel((x+3, y+3), (0x00, 0xFF, 0x0))
+        except IndexError:
+            None """
+
+with open("../dots.csv", "w") as file:
     writer = csv.DictWriter(
         file, fieldnames=["x", "y", "score"])
     writer.writeheader()
-    for i in range(0, colored.__len__()):
-        c = colored[i]
-        """ cd = colored_dirs[i] """
-        writer.writerow({
-            "x": c[0],
-            "y": c[1],
-            "score": 1,
-        })
-
-
-"""for p in colored:
-    draw.arc([
-        p[0],
-        p[1],
-        p[0] + brush_size,
-        p[1] + brush_size,
-    ],
-        0,
-        360)
-im.show()
-"""
+    for i in tqdm(range(0, pixelcount), desc="Writing file"):
+        if bitmap[i] == True:
+            (x, y) = as_coord(i)
+            im2.putpixel((x, y), 1)
+            writer.writerow({
+                "x": x,
+                "y": y,
+                "score": 1,
+            })
